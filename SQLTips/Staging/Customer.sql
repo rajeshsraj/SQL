1ef1607b7f1507f99bb4157077871ed85351e716
rajeshsraj <rajesh.gradate@gmail.com>
2021-02-13 13:40:32 +0530

    created a table customer

---
+++ SQLTips/Staging/Customer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[customer_id] INT NOT NULL PRIMARY KEY, 
+    [first_name] NCHAR(20) NOT NULL, 
+    [last_name] NCHAR(20) NOT NULL, 
+    [email] NVARCHAR(30) NOT NULL, 
+    [address] NVARCHAR(50) NOT NULL, 
+    [city] NCHAR(20) NOT NULL, 
+    [state] NCHAR(20) NOT NULL, 
+    [zip] NVARCHAR(15) NOT NULL
+)
